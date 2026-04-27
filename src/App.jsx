730fc97aa6f8620d90a43ccfec2c692eae35711b
Moheb-Ashraf <moheba363@gmail.com>
2026-04-27 10:19:04 +0300

    edit app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,56 +8,8 @@ const GPACalculator = lazy(() => import('./pages/GPACalculator'))
 const PercentageCalculator = lazy(() => import('./pages/PercentageCalculator'))
 const FinalGradeCalculator = lazy(() => import('./pages/FinalGradeCalculator'))
 const Home = lazy(() => import('./pages/Home'))
-const SitemapPage = lazy(() => import('./pages/SitemapPage'))
-const RobotsPage = lazy(() => import('./pages/RobotsPage'))
 
 export default function App() {
-  const path = window.location.pathname
-
-  if (path === '/sitemap.xml') {
-    const xml = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
-        xmlns:xhtml="http://www.w3.org/1999/xhtml">
-  <url>
-    <loc>https://student-tools-two.vercel.app/</loc>
-    <lastmod>2025-01-01</lastmod>
-    <changefreq>weekly</changefreq>
-    <priority>1.0</priority>
-  </url>
-  <url>
-    <loc>https://student-tools-two.vercel.app/gpa-calculator</loc>
-    <lastmod>2025-01-01</lastmod>
-    <changefreq>monthly</changefreq>
-    <priority>0.9</priority>
-  </url>
-  <url>
-    <loc>https://student-tools-two.vercel.app/percentage-calculator</loc>
-    <lastmod>2025-01-01</lastmod>
-    <changefreq>monthly</changefreq>
-    <priority>0.9</priority>
-  </url>
-  <url>
-    <loc>https://student-tools-two.vercel.app/final-grade-calculator</loc>
-    <lastmod>2025-01-01</lastmod>
-    <changefreq>monthly</changefreq>
-    <priority>0.9</priority>
-  </url>
-</urlset>`
-
-    document.open('application/xml')
-    document.write(xml)
-    document.close()
-    return null
-  }
-
-  if (path === '/robots.txt') {
-    const txt = `User-agent: *\nAllow: /\nSitemap: https://student-tools-two.vercel.app/sitemap.xml`
-    document.open('text/plain')
-    document.write(txt)
-    document.close()
-    return null
-  }
-
   return (
     <div className="min-h-screen flex flex-col bg-ink-950">
       <Navbar />
